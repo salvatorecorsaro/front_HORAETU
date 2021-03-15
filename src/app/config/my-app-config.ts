af477e19b1554d953f50ef9621030783ed17b365
scorsaro <salvatore.corsaro@live.com>
2021-03-15 10:48:21 +0100

    final deploy

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
   oidc: {
     clientId: '0oab977umE99iyAT25d6',
     issuer: 'https://dev-53856977.okta.com/oauth2/default',
-    redirectUri: 'http://horaetu.shop/login/callback',
+    redirectUri: 'https://139.162.193.183:80/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 
